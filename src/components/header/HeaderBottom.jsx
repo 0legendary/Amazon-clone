ddab3f8be5a44d58806d92b5107b2da3c5e6d618
Alen.m <alenmullassery123@gmail.com>
2023-05-24 17:10:51 +0530

    Effects using framer motion npm on NavBar

--- src/components/header/HeaderBottom.jsx
+++ src/components/header/HeaderBottom.jsx
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState,useRef } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
+import {motion} from "framer-motion"
 import CloseIcon from '@mui/icons-material/Close';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SideNavContent from './SideNavContent';
 
 function HeaderBottom() {
+  const ref = useRef();
   const [sideBar, setsideBar] = useState(false)
+  useEffect(()=>{
+    document.body.addEventListener("click",(e)=>{
+      if(e.target.contains(ref.current)){
+        setsideBar(false)
+      }
+    })
+  },[ref,sideBar])
   return (
     <div className='header-bottom'>
       {/* List Items */}
@@ -25,19 +35,61 @@ function HeaderBottom() {
         <li className='header-effect'>New Releases</li>
         <li className='header-effect'>Coupons</li>
         <li className='header-effect'>Home & Kitchen</li>
+        <li className='header-effect bottom-list'>New Launches From Mobiles, Electronics & more | Shop now</li>
       </ul>
       {/* Side NavBar */}
 
       {
         sideBar && (
           <div className="side-bar">
-            <div className='side-bar-items'>
-              <div className='side-bar-border'>
-                <div>
-                  hai
+            <motion.div ref={ref} initial={{x:-500,opacity:1}} animate={{x:0,opacity:1}} 
+                transition={{duration:.5}}className='side-bar-items'>
+              <div  className='side-bar-border'>
+                <div className='inner-side-bar'>
+                  <AccountCircleIcon />
+                  <h3 className='side-nav-acc'>Hello, Sign In</h3>
                 </div>
+                
+
+                <SideNavContent
+                  title="Trending"
+                  one="Best Sellers"
+                  two="New Relases"
+                  three="Movie & Shakers"
+                />
+                <SideNavContent
+                  title="Digital Content And Devices"
+                  one="Amazon miniTv-FREE entertainment"
+                  two="Echo & Alexa"
+                  three="Fire TV"
+                  four="Kindle E-Readers & eBooks"
+                  five="Audible Audiobooks"
+                  six="Amazon Prime Video"
+                  seven="Amazon Prime Music"
+                />
+                <SideNavContent
+                  title="Shop By Category"
+                  one="Mobiles, Computers"
+                  two="TV, Appliances, Electronics"
+                  three="Men's Fashion"
+                  four="Women's Fashion"
+                />
+                <SideNavContent
+                  title="Programs & Features"
+                  one="Gift Cards & Mobile Recharges"
+                  two="Flight Tickets"
+                  three=" Clearance store"
+                />
+                <SideNavContent
+                  title="Help & Settings"
+                  one="Your Account"
+                  two="Customer Service"
+                  three="Sign Out"
+                />
               </div>
-            </div>
+
+              <span onClick={()=>setsideBar(false)} className="close-icon"><CloseIcon className='close-icon-side-bar'/></span>
+            </motion.div>
           </div>
         )
       }
--- src/components/header/SideNavContent.jsx
+++ src/components/header/SideNavContent.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
-function SideNavContent() {
+function SideNavContent({ title, ...items }) {
+  const itemList = Object.entries(items);
+
   return (
-    <div>
-      
+    <div className='bottom-line'>
+      <h3 className='nav-titles'>{title}</h3>
+      <ul>
+        {itemList.map(([key, text]) => (
+          <li className='nav-lists' key={key}>
+            {text}
+            {shouldShowIcon(title, key) && <span><KeyboardArrowRightIcon /></span>}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default SideNavContent
+function shouldShowIcon(title, key) {
+  if (title === 'Trending') {
+    return key !== 'one' && key !== 'two' && key !=='three';
+  } else if (title === 'Digital Content And Devices') {
+    return key !== 'one';
+  } else if (title === 'Programs & Features') {
+    return key !== 'two' && key !== 'three';
+  } else if (title === 'Help & Settings') {
+    return key !== 'one' && key !== 'two' && key !=='three';
+  } else {
+    return true; // Display the icon for all items in other sections
+  }
+}
+
+export default SideNavContent;
+
